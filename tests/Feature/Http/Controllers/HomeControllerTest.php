cb0909cce4767bbd653f2cc777eba52eba67208c
lulzshadowwalker <lulzshadowwalker@gmail.com>
2025-06-26 15:18:53 +0300

    Added a smoke test to the home page

---
+++ tests/Feature/Http/Controllers/HomeControllerTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Tests\Feature\Http\Controllers;
+
+use App\Models\Collection;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class HomeControllerTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function test_it_renders_the_page(): void
+    {
+        Collection::factory()->count(3)->create();
+        $this->get(route('home.index'))->assertOk();
+    }
+}
