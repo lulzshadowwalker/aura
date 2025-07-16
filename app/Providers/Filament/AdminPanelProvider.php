0b4fcf912a0d9aeac84c2726ce141e10d48778ad
lulzshadowwalker <lulzshadowwalker@gmail.com>
2025-07-16 14:22:27 +0300

    Installed outerweb-translatable-fields Filament plugin

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -17,6 +17,7 @@ use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
 use Illuminate\Routing\Middleware\SubstituteBindings;
 use Illuminate\Session\Middleware\StartSession;
 use Illuminate\View\Middleware\ShareErrorsFromSession;
+use Outerweb\FilamentTranslatableFields\Filament\Plugins\FilamentTranslatableFieldsPlugin;
 
 class AdminPanelProvider extends PanelProvider
 {
@@ -53,6 +54,12 @@ class AdminPanelProvider extends PanelProvider
             ])
             ->authMiddleware([
                 Authenticate::class,
+            ])
+            ->plugins([
+                FilamentTranslatableFieldsPlugin::make()->supportedLocales([
+                    'en' => 'English',
+                    'ar' => 'Arabic',
+                ]),
             ]);
     }
 }
