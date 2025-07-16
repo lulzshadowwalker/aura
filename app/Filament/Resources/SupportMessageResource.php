de7c7d44f55f63423de6d18bb7492841b4b8b057
lulzshadowwalker <lulzshadowwalker@gmail.com>
2025-07-16 14:28:50 +0300

    Added support messages to admin panel

---
+++ app/Filament/Resources/SupportMessageResource.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\SupportMessageResource\Pages;
+use App\Filament\Resources\SupportMessageResource\RelationManagers;
+use App\Models\SupportMessage;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class SupportMessageResource extends Resource
+{
+    protected static ?string $model = SupportMessage::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('name')
+                    ->required()
+                    ->maxLength(255),
+                Forms\Components\TextInput::make('email')
+                    ->email()
+                    ->required()
+                    ->maxLength(255),
+                Forms\Components\Textarea::make('message')
+                    ->required()
+                    ->columnSpanFull(),
+
+                Forms\Components\Select::make('customer_id')
+                    ->label('Customer')
+                    ->relationship('customer.user', 'name')
+                    ->searchable()
+                    ->required()
+                    ->preload()
+                    ->disabled()
+                    ->columnSpanFull(),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('name')
+                    ->label('Name')
+                    ->searchable(),
+
+                Tables\Columns\TextColumn::make('email')
+                    ->label('Email')
+                    ->searchable(),
+
+                Tables\Columns\TextColumn::make('customer.user.name')
+                    ->label('Customer')
+                    ->numeric()
+                    ->sortable(),
+
+                Tables\Columns\TextColumn::make('created_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+
+                Tables\Columns\TextColumn::make('updated_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\ViewAction::make()->slideOver(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListSupportMessages::route('/'),
+            'edit' => Pages\EditSupportMessage::route('/{record}/edit'),
+        ];
+    }
+}
