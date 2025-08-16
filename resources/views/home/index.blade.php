c859bb07cb577e12e2fcd78e61a37ee861b96271
lulzshadowwalker <lulzshadowwalker@gmail.com>
2025-08-16 22:20:50 +0300

    feat: update home view hero section with refined text and branding enhancements

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -12,10 +12,11 @@
         <div class="bg-gradient-to-t from-black/60 to-black/0 absolute inset-0 z-10"></div>
         <div class="text-neutral-content text-center relative z-10">
             <div class="max-w-md mx-auto">
-                <h1 class="mb-5 text-5xl font-bold drop-shadow-lg">Hello there</h1>
+                <h1 class="mb-5 text-5xl font-bold drop-shadow-lg">
+                    A Breeze of Luxury
+                </h1>
                 <p class="mb-5 drop-shadow-lg">
-                    Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
-                    quasi. In deleniti eaque aut repudiandae et a id nisi.
+                    Northwind perfumes blend luxury and authenticity to create an unforgettable fragrance experie
                 </p>
                 <button class="btn btn-primary btn-lg shadow-lg">Get Started</button>
             </div>
