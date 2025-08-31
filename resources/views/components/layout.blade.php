138ebce52195b20c3e11bbf8c306d6ccf1e32ab8
lulzshadowwalker <lulzshadowwalker@gmail.com>
2025-08-31 15:06:20 +0300

    Disable Google Analytics for local development

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -1,24 +1,26 @@
 <!DOCTYPE html>
 <html lang="{{ app()->getLocale() }}" dir="{{ app()->getLocale() === 'ar' ? 'rtl' : 'ltr' }}" data-theme="cookie"
-      class="scroll-smooth">
+    class="scroll-smooth">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <!-- Google tag (gtag.js) -->
-    <script async src="https://www.googletagmanager.com/gtag/js?id=G-BTWQ5QP28W"></script>
-    <script>
-        window.dataLayer = window.dataLayer || [];
+    @unless (app()->environment('local'))
+        <!-- Google tag (gtag.js) -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-BTWQ5QP28W"></script>
+        <script>
+            window.dataLayer = window.dataLayer || [];
 
-        function gtag() {
-            dataLayer.push(arguments);
-        }
+            function gtag() {
+                dataLayer.push(arguments);
+            }
 
-        gtag('js', new Date());
+            gtag('js', new Date());
 
-        gtag('config', 'G-BTWQ5QP28W');
-    </script>
+            gtag('config', 'G-BTWQ5QP28W');
+        </script>
+    @endunless
 
     <title>{{ isset($title) && $title ? $title . ' | ' . config('app.name') : config('app.name') }}</title>
     <link rel="icon" href="{{ asset('favicon.ico') }}" type="image/x-icon">
@@ -27,27 +29,27 @@
 </head>
 
 <body class="min-h-screen flex flex-col">
-<x-marquee :text="__('app.marquee')"/>
+    <x-marquee :text="__('app.marquee')" />
 
-<x-header :collections="$collections"/>
+    <x-header :collections="$collections" />
 
-<main class="flex-1"> {{ $slot }} </main>
-<x-flash-messages/>
+    <main class="flex-1"> {{ $slot }} </main>
+    <x-flash-messages />
 
-<x-footer :collections="$collections"/>
+    <x-footer :collections="$collections" />
 
-<x-cart-fab/>
+    <x-cart-fab />
 
-<!-- FontAwesomeIcons -->
-<script src="https://kit.fontawesome.com/a51f251d24.js" crossorigin="anonymous"></script>
+    <!-- FontAwesomeIcons -->
+    <script src="https://kit.fontawesome.com/a51f251d24.js" crossorigin="anonymous"></script>
 
-<!-- NOTE: Use fontawesome going forward as we will deprecate lucide icons at some point -->
-<!-- Lucide Icons -->
-<script src="https://unpkg.com/lucide@latest"></script>
-<script>
-    lucide.createIcons();
-</script>
-@stack('scripts')
+    <!-- NOTE: Use fontawesome going forward as we will deprecate lucide icons at some point -->
+    <!-- Lucide Icons -->
+    <script src="https://unpkg.com/lucide@latest"></script>
+    <script>
+        lucide.createIcons();
+    </script>
+    @stack('scripts')
 </body>
 
 </html>
